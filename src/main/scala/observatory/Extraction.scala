1e8e4cfca1aa9ed02a27ccb8e0d810a1b2a25ee9
Zi Jing <bigzijing@gmail.com>
2021-04-18 23:36:28 +0800

    Fix file parsing

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -1,14 +1,12 @@
 package observatory
 
 import java.time.LocalDate
-
 import scala.io.{BufferedSource, Source}
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction extends ExtractionInterface {
-
   type Fahrenheit = Double
   case class Station(STN: String, WBAN: String)
 
@@ -19,32 +17,36 @@ object Extraction extends ExtractionInterface {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    val yearFileIterable: Iterable[String] = loadSourceFromFile(year.toString)
-    val stationsFileIterable: Iterable[String] = loadSourceFromFile(stationsFile)
+    val temperatureFileIterable: Iterator[String] = loadSourceFromFile(temperaturesFile)
+    val stationsFileIterable: Iterator[String] = loadSourceFromFile(stationsFile)
 
-    val temperatureByStation: Iterable[(Station, LocalDate, Temperature)] = yearFileIterable.map(_.split(",")).collect {
-      case Array(stn, wban, m, d, f) => (Station(stn, wban), LocalDate.of(year, m.toInt, d.toInt), fahrenheit2Celsius(f.toDouble))
+    val temperatureByStation: Iterator[(Station, LocalDate, Temperature)] = temperatureFileIterable.map(_.split(",")).collect {
+      case tokens @Array(stn, wban, m, d, f) if checkArray(tokens) && tokens.size == 4 => (Station(stn, wban), LocalDate.of(year, m.toInt, d.toInt), fahrenheit2Celsius(f.toDouble))
     }
     val stationsToLocationMap: Map[Station, Location] = stationsFileIterable.map(_.split(",")).collect {
-      case Array(stn, wban, lat, long) => (Station(stn, wban), Location(lat.toDouble, long.toDouble))
+      case tokens @Array(stn, wban, lat, long) if checkArray(tokens) && tokens.size == 5 => (Station(stn, wban), Location(lat.toDouble, long.toDouble))
     }.toMap
 
     temperatureByStation.collect {
       case line if stationsToLocationMap.contains(line._1) => (line._2, stationsToLocationMap(line._1) ,line._3)
-    }
+    }.toIterable
   }
 
-  def loadSourceFromFile(filePath: String): Iterable[String] = Source.fromInputStream(getClass.getResourceAsStream(s"/$filePath"), "utf-8").getLines().toIterable
+  def checkArray(tokens: Array[String]): Boolean = !tokens.contains("")
 
-  def fahrenheit2Celsius(degreesInF: Fahrenheit): Temperature = (degreesInF - 30.0) * (5/9.0)
+  def loadSourceFromFile(fileName: String): Iterator[String] = Source.fromInputStream(getClass.getResourceAsStream(fileName)).getLines()
+
+  def fahrenheit2Celsius(degreesInF: Fahrenheit): Temperature = (degreesInF - 30.0) * (5.0/9.0)
 
   /**
     * @param records A sequence containing triplets (date, location, temperature)
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    records.groupBy(_._2).mapValues { iter =>
-      iter.map(_._3).sum / iter.size
+    records.groupBy(_._2).map { iter =>
+      val location = iter._1
+      val tempIter = iter._2.map(_._3)
+      (location, tempIter.sum / tempIter.size)
     }
   }
 
