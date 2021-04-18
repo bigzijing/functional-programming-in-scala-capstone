d9064df243cbcb5a1f8f7cad5713467b92199f85
Zi Jing <bigzijing@gmail.com>
2021-04-18 23:52:02 +0800

    Fix error as stations with only one of STN/WBAN are actually valid

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -21,10 +21,10 @@ object Extraction extends ExtractionInterface {
     val stationsFileIterable: Iterator[String] = loadSourceFromFile(stationsFile)
 
     val temperatureByStation: Iterator[(Station, LocalDate, Temperature)] = temperatureFileIterable.map(_.split(",")).collect {
-      case tokens @Array(stn, wban, m, d, f) if checkArray(tokens) && tokens.size == 5 => (Station(stn, wban), LocalDate.of(year, m.toInt, d.toInt), fahrenheit2Celsius(f.toDouble))
+      case tokens @Array(stn, wban, m, d, f) if tokens.size == 5 => (Station(stn, wban), LocalDate.of(year, m.toInt, d.toInt), fahrenheit2Celsius(f.toDouble))
     }
     val stationsToLocationMap: Map[Station, Location] = stationsFileIterable.map(_.split(",")).collect {
-      case tokens @Array(stn, wban, lat, long) if checkArray(tokens) && tokens.size == 4 => (Station(stn, wban), Location(lat.toDouble, long.toDouble))
+      case tokens @Array(stn, wban, lat, long) if lat != "" && long != "" && tokens.size == 4 => (Station(stn, wban), Location(lat.toDouble, long.toDouble))
     }.toMap
 
     temperatureByStation.collect {
