9457cb6c41b0d1de3a211fd31e92d1ee95fecceb
zijing93 <zijing@leadiq.com>
2021-04-19 23:17:31 +0800

    Write flow for predictTemperature functions

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -12,16 +12,30 @@ object Visualization extends VisualizationInterface {
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+  def predictTemperature(temperatures: Iterable[(Location, Temperature)],
+                         location: Location): Temperature = {
+    val distances =
+      temperatures.map(row => (calcDistance(row._1, location), row._2))
   }
+
+  def calcDistance(loc1: Location, loc2: Location): Double = {
+    if (loc1 == loc2) 0
+    else if (areAntipodes(loc1, loc2)) {
+      ???
+    } else {
+      ???
+    }
+  }
+
+  def areAntipodes(loc1: Location, loc2: Location): Boolean = {}
 
   /**
     * @param points Pairs containing a value and its associated color
     * @param value The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
-  def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
+  def interpolateColor(points: Iterable[(Temperature, Color)],
+                       value: Temperature): Color = {
     ???
   }
 
@@ -30,9 +44,9 @@ object Visualization extends VisualizationInterface {
     * @param colors Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
-  def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
+  def visualize(temperatures: Iterable[(Location, Temperature)],
+                colors: Iterable[(Temperature, Color)]): Image = {
     ???
   }
 
 }
-
